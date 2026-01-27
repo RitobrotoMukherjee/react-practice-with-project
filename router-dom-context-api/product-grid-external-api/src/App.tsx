af72c0feecbaf4f8be15cd06449aebaa3f18fef8
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2026-01-27 12:59:01 +0530

    product grid with router dom initial set-up

---
+++ router-dom-context-api/product-grid-external-api/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-zinc-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen">
+        <h1 className="text-3xl font-bold underline p-4">
+          Hello Router!
+        </h1>
+    </div>
+  )
+}
+
+export default App
